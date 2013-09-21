3bf96bc6ce09f8763046885b8bb50492d2200e2c
Brendan Reddy-Best <b.d.reddybest@gmail.com>
2013-09-20 18:16:39 -0700

    Create morse.py

---
+++ morse.py
@@ -0,0 +1,92 @@
+morse_alphabet = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    " ": "/"
+}
+
+
+def ascii_to_morse(ascii_string):
+    """ 
+    Given a normal sentence, this will return the morse code for it, with 
+    letters seperated by spaces and words seperated by / 
+    """
+    morse = ""
+    for char in ascii_string:
+        morse += morse_alphabet[char] + " "
+    return morse
+
+def morse_to_ascii(morse_string):
+    """
+    Expects a string of morse characters seperated by spaces with the words 
+    seperated by /
+    """
+    ascii = ""
+    for code in morse_string.split():
+        translated = None
+        # backwards dictionary lookup
+        for key in morse_alphabet.keys():
+            if (morse_alphabet[key] == code):
+                translated = key
+        assert translated is not None
+        ascii += translated
+    return ascii
